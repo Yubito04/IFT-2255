2c8312c543c9ae4a60527679ad753188b9602d7e
Yubito04 <ayoubbencheikh34@gmail.com>
2025-12-01 18:20:08 -0500

    Update ComparisonService.java

--- rest-api/src/main/java/com/diro/ift2255/service/ComparisonService.java
+++ rest-api/src/main/java/com/diro/ift2255/service/ComparisonService.java
@@ -1,1 +1,53 @@
+package com.diro.ift2255.service;
+
+import com.diro.ift2255.model.Course;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class ComparisonService {
+
+    private final CourseService courseService;
+
+    public ComparisonService(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    public ComparisonResult compareCourses(List<String> courseIds) {
+        List<Course> foundCourses = new ArrayList<>();
+        int totalWorkload = 0;
+
+        for (String id : courseIds) {
+            Optional<Course> courseOpt = courseService.getCourseById(id);
+            if (courseOpt.isPresent()) {
+                Course c = courseOpt.get();
+                foundCourses.add(c);
+
+                int credits = c.getCredits() > 0 ? c.getCredits() : 3; // valeur par défaut
+                totalWorkload += credits * 3;
+            }
+        }
+
+        return new ComparisonResult(foundCourses, totalWorkload);
+    }
+
+    public static class ComparisonResult {
+        private List<Course> courses;
+        private int totalWorkload;
+
+        public ComparisonResult(List<Course> courses, int totalWorkload) {
+            this.courses = courses;
+            this.totalWorkload = totalWorkload;
+        }
+
+        public List<Course> getCourses() {
+            return courses;
+        }
+
+        public int getTotalWorkload() {
+            return totalWorkload;
+        }
+    }
+}
 
